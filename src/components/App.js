24dbda227becb8923f1ae72e61fcda2a2ebb4e7d
Cody Brunner <rockchalkwushock@icloud.com>
2016-10-21 01:51:27 -0500

    V1.0: All initial files, directories, dependencies, & scripts added. Initial render of App component to DOM passing only component in render function a success.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='test-run #1'>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,37 @@
+import path from 'path';
+import webpack from 'webpack';
+import packageData from './package.json';
+
+let minify = process.argv.indexOf('--minify') != -1;
+let filename = [packageData.name, packageData.version, 'js'];
+let plugins = [];
+
+if (minify) {
+    filename.splice(filename.length - 1, 0, 'min');
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+export default {
+    entry: path.resolve(__dirname, packageData.main),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: filename.join('.'),
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    plugins: plugins,
+    resolve: {
+    extensions: ['', '.js', '.jsx']
+    },
+};
