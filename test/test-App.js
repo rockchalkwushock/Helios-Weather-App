6625f76da567429cc7fd7aa582fbe0c95bb5bf3b
Cody Brunner <rockchalkwushock@icloud.com>
2016-10-21 02:19:55 -0500

    V1.3: Added test-App.js. Wrote & ran tests on App Component. Added .travis.yml. Passed test loacally, pushing to Travis CI for first build.

---
+++ test/test-App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import App from '../src/components/App';
+
+describe('<App />', () => {
+  it('should have the name: App', () => {
+    const TestComponent = () => <App />;
+    const wrapper = shallow(<TestComponent />);
+    expect(wrapper.name()).to.equal('App');
+  });
+  it('should be a div', () => {
+    const wrapper = shallow(<div/>);
+    expect(wrapper.name()).to.equal('div');
+  });
+  it('should have className: test', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div').hasClass('test')).to.equal(true);
+  });
+  it('should have one children', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1')).to.have.length(1);
+  });
+});
