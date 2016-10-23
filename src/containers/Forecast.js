578d6660407ced09f1ec6a3806e5ed14aa382a0f
Cody Brunner <rockchalkwushock@icloud.com>
2016-10-23 12:57:50 -0500

    V5.6: Created conversions_2.0.js. All conversion are now stored in an object and accessed as methods to the object. With @rgnet1's help the appending issue has been resolved.

--- src/containers/Forecast.js
+++ src/containers/Forecast.js
@@ -1,54 +1,39 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {degToCard, tempConverter, timeStampConverter, windSpeedConverter} from '../conversions/conversions';
+import {unitConverter} from '../conversions/conversions_2.0.js';
 
-export class Forecast extends Component {
-    _renderWeather(cityData) {
-      // Each const is an [5]
-      const time = cityData.list.slice(0, 5).map(weather => weather.dt).map(timeStampConverter);
-      const temp = cityData.list.slice(0, 5).map(weather => weather.main.temp).map(tempConverter);
-      const speed = cityData.list.slice(0, 5).map(weather => weather.wind.speed).map(windSpeedConverter);
-      const direction = cityData.list.slice(0, 5).map(weather => weather.wind.deg).map(degToCard);
 
-      // let forecastObject = {
-      //   time,
-      //   temp,
-      //   speed,
-      //   direction
-      // };
-      //
-      // console.log(forecastObject);
+class Forecast extends Component {
+  _renderForecast(cityData){
+    return cityData.list.slice(0,5).map(data => {
+      return (
+        <tr key={data.dt}>
+          <td>{unitConverter.toTime(data.dt)}</td>
+          <td>{unitConverter.toTemp(data.main.temp)}</td>
+          <td>{unitConverter.toSpeed(data.wind.speed)} {unitConverter.toDegree(data.wind.deg)}</td>
+        </tr>
+      );
+    });
+  }
 
-        return (
-          <tr key={time}>
-              <td>{time}</td>
-              <td>{temp} F</td>
-              <td>{speed} mph {direction}</td>
+  render(){
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <td>Time</td>
+            <td>Temp</td>
+            <td>Wind</td>
           </tr>
-        );
-    }
-    render() {
-        let listTable = [];
-        if (this.props.forecast) {
-            listTable = this.props.forecast.map(this._renderWeather);
-        }
-
-        return (
-          <table className="table table-hover">
-              <thead>
-                  <tr>
-                      <th>Time</th>
-                      <th>Temp</th>
-                      <th>Wind</th>
-                  </tr>
-              </thead>
-              <tbody>
-                {this.props.forecast.map(this._renderWeather)}
-              </tbody>
-          </table>
-        );
-    }
+        </thead>
+        <tbody>
+          {this.props.forecast.map(this._renderForecast)}
+        </tbody>
+      </table>
+    );
+  }
 }
+
 
 function mapStateToProps({forecast}) {
     return {forecast};
---
+++ src/conversions/conversions_2.0.js
@@ -0,0 +1,64 @@
+import _ from 'lodash';
+
+export const unitConverter = {
+  toDegree: (deg) => {
+    switch (true) {
+      case (deg>11.25 && deg<33.75): return 'NNE';
+      case (deg>33.75 && deg<56.25): return 'NE';
+      case (deg>56.25 && deg<78.75): return 'ENE';
+      case (deg>78.75 && deg<101.25): return 'E';
+      case (deg>101.25 && deg<123.75): return 'ESE';
+      case (deg>123.75 && deg<146.25): return 'SE';
+      case (deg>146.25 && deg<168.75): return 'SSE';
+      case (deg>168.75 && deg<191.25): return 'S';
+      case (deg>191.25 && deg<213.75): return 'SSW';
+      case (deg>213.75 && deg<236.25): return 'SW';
+      case (deg>236.25 && deg<258.75): return 'WSW';
+      case (deg>258.75 && deg<281.25): return 'W';
+      case (deg>281.25 && deg<303.75): return 'WNW';
+      case (deg>303.75 && deg<326.25): return 'NW';
+      case (deg>326.25 && deg<348.75): return 'NNW';
+      case (deg>348.75 && deg<11.25): return 'N';
+    }
+  },
+
+  toPressure: (hPa) => {
+    const hPaConversion = 33.8638866667;
+    return _.round(hPa / hPaConversion, 2);
+  },
+
+  toSpeed: (metersPerSecond) => {
+    const metersPerSecondConversion =  0.44704;
+    return _.round(metersPerSecond / metersPerSecondConversion);
+  },
+
+  toTemp: (kelvin) => {
+    const farenheit = _.round(kelvin * (9/5) - 459.67);
+    return farenheit;
+  },
+
+  toTime: (utcTime) => {
+    const hours = new Date(utcTime * 1000).getHours();
+    let minutes = new Date(utcTime * 1000).getMinutes();
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    switch (true) {
+      case (hours === 13): return `1:${minutes} pm`;
+      case (hours === 14): return `2:${minutes} pm`;
+      case (hours === 15): return `3:${minutes} pm`;
+      case (hours === 16): return `4:${minutes} pm`;
+      case (hours === 17): return `5:${minutes} pm`;
+      case (hours === 18): return `6:${minutes} pm`;
+      case (hours === 19): return `7:${minutes} pm`;
+      case (hours === 20): return `8:${minutes} pm`;
+      case (hours === 21): return `9:${minutes} pm`;
+      case (hours === 22): return `10:${minutes} pm`;
+      case (hours === 23): return `11:${minutes} pm`;
+      case (hours === 24): return `12:${minutes} pm`;
+    }
+    return `${hours}:${minutes} am`;
+  },
+};
