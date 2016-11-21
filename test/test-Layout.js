847140b1f188013960082cf0ecd1233c087ed39b
Cody Brunner <rockchalkwushock@icloud.com>
2016-11-20 23:17:44 -0600

    Updated tests.

--- test/test-Layout.js
+++ test/test-Layout.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import App from '../src/components/App';
+import Layout from '../src/components/Layout';
 
-describe('<App />', () => {
-  it('should have the name: App', () => {
-    const TestComponent = () => <App />;
+describe('<Layout />', () => {
+  it('should have the name: Layout', () => {
+    const TestComponent = () => <Layout />;
     const wrapper = shallow(<TestComponent />);
-    expect(wrapper.name()).to.equal('App');
+    expect(wrapper.name()).to.equal('Layout');
   });
   it('should be a div', () => {
     const wrapper = shallow(<div/>);
     expect(wrapper.name()).to.equal('div');
   });
-  it('should have className: test', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('div').hasClass('test')).to.equal(true);
+  it('should have className: layout', () => {
+    const wrapper = shallow(<Layout />);
+    expect(wrapper.find('div').hasClass('layout')).to.equal(true);
   });
   it('should have four children', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Layout />);
     expect(wrapper.find('Navbar')).to.have.length(1);
     expect(wrapper.find('SearchBarContainer')).to.have.length(0);
     expect(wrapper.find('HeaderContainer')).to.have.length(0);
