d40e3470872341bb1dbb567c9ef0112c20f136be
Cody Brunner <rockchalkwushock@icloud.com>
2017-06-04 08:38:50 -0500

    chore(root): Added eslint, prettier, lint-staged, husky
    [skip-ci]

--- package-scripts.js
+++ package-scripts.js
@@ -1,8 +1,24 @@
+const npsUtils = require('nps-utils')
+
+const { rimraf, series } = npsUtils
+
 module.exports = {
   scripts: {
+    clean: {
+      description: 'Clean repository of generated directories & files.',
+      script: series(rimraf('.next'), rimraf('coverage'))
+    },
     commit: {
       description: 'Run commitizen-cli for creating clean commit messages.',
       script: 'git cz'
+    },
+    lint: {
+      description: 'Lint code base.',
+      default: 'eslint __tests__ components lib pages',
+      fix: {
+        description: 'Lint & fix errors.',
+        script: series.nps('lint --fix')
+      }
     }
   }
 }
