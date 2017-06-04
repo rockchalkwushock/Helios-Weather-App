741b9e08d15ea5df24b63ea9aa5fe293c3dcbeec
Cody Brunner <rockchalkwushock@icloud.com>
2017-06-04 14:47:51 -0500

    style(root): Setup some of the basic layout using glamor & glamorous [skip ci]

---
+++ components/Wrapper.js
@@ -0,0 +1,12 @@
+import glamorous from 'glamorous'
+
+const Wrapper = glamorous.div({
+  backgroundColor: '#123456',
+  display: 'flex',
+  flexDirection: 'column',
+  margin: 'auto',
+  maxWidth: '50em',
+  minHeight: '100vh'
+})
+
+export default Wrapper
